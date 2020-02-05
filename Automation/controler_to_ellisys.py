ab158289371ac2e42622832b08df97bfe4148e82
alexandre.dumur@epfl.ch <alexandre.dumur@epfl.ch>
2020-02-05 16:35:25 +0100

    Capture pipeline tested and seemed to work ok

--- Automation/controler_to_ellisys.py
+++ Automation/controler_to_ellisys.py
@@ -9,18 +9,13 @@ PORT = 65432        # The port used by the server
 
 
 def send_instruction(instruction):
-    instructions = ["start ellisys", "stop ellisys", "start capture", "save capture"]
-    if instruction not in instructions:
-        print("falls instruction")
+    instructions = ["open ellisys", "close ellisys", "start capture", "stop capture"]
+    if instruction not in instructions and instruction[:12] != "save capture" and instruction[:13] != "start capture":
         raise AttributeError("Should be either: " + ", ".join(instructions))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    s.sendall(instruction)
+    s.sendall(str.encode(instruction))
     data = s.recv(1024)
     s.close()
-
+    print(" -> " + data.decode('utf-8'))
     return data
-
-data = send_instruction("start ellisys")
-print repr(data)
-print type(data)
